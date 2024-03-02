e65e653307cdd1069008e621e74ba615bd93f713
quyenvdt <quyenvdt@nongnghiepso.com>
2024-03-02 16:32:37 +0700

    first commit

---
+++ components/TopBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-[rgb(68,185,124)] py-3 px-3">
+    <div
+      class="max-w-6xl mx-auto block sm:flex sm:justify-between sm:items-center h-full text-white text-[15px] font-inter"
+    >
+      <div>Hà Nội, Việt Nam</div>
+      <div>
+        <span class="mr-2 block sm:inline">Email: Office@nongnghiepso.com</span>
+        <span>Liên hệ: 0966 688 650</span>
+      </div>
+    </div>
+  </div>
+</template>
